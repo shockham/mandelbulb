ce38200bef0d2e606ba744934b19fb595a0a99cc
shockham <samuel.hockham@gmail.com>
2018-04-05 12:24:46 +0100

    Include shader as string in one-liner

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use caper::utils::demo;
 use std::str;
 
 fn main() {
-    let shader = include_bytes!("frag.glsl");
-    let frag_str = str::from_utf8(shader).unwrap();
+    let frag_str = str::from_utf8(include_bytes!("frag.glsl")).unwrap();
     demo(frag_str);
 }
